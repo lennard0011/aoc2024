42bc0570004916b0a6b74ed29844d2036f03ae64
Lennard van der Plas <lennardplas@gmail.com>
2024-12-10 21:34:44 +0100

    feat(day9): part 2

--- 9/main.py
+++ 9/main.py
@@ -47,6 +47,65 @@ def reduce_string(file_string):
     return cleaned_file[:pointer_left + 1]
 
 
+def find_substring_start(file_string, index):
+    search_index = index
+    char = file_string[search_index]
+    while (True):
+        if file_string[search_index - 1] != char:
+            break
+        search_index -= 1 
+    return search_index
+
+def find_substring_finish(file_string, index):
+    search_index = index
+    char = file_string[search_index]
+    while (True):
+        if file_string[search_index + 1] != char:
+            break
+        search_index += 1 
+    return search_index
+
+
+def reduce_string_part2(file_string):
+    pointer_left = 0
+    pointer_right = len(file_string) - 1
+
+    cleaned_file = copy.deepcopy(file_string)
+
+    while(True):
+        print(pointer_right)
+        if pointer_right <= 0:
+            break
+        if cleaned_file[pointer_right] == ".":
+            pointer_right -= 1
+            continue
+
+        block_start = find_substring_start(cleaned_file, pointer_right)
+        block_length = pointer_right - block_start + 1
+        pointer_left = 0
+        while(True):
+            if pointer_left == pointer_right:
+                pointer_right = block_start - 1
+                break
+
+            if cleaned_file[pointer_left] != ".":
+                pointer_left += 1
+                continue
+
+            space_finish = find_substring_finish(cleaned_file, pointer_left)
+            space_length = space_finish - pointer_left + 1
+
+            if block_length <= space_length: 
+                cleaned_file[pointer_left:pointer_left + block_length] = cleaned_file[block_start:pointer_right + 1]
+                cleaned_file[block_start:pointer_right + 1] = ["." for i in range(block_length)]
+                pointer_right = block_start - 1 
+                break
+            else:
+                pointer_left = space_finish + 1
+    
+    return cleaned_file
+
+
 def calculate_check_sum(cleaned_string):
     sum = 0
     for i in range(len(cleaned_string)):
@@ -65,10 +124,13 @@ def main(part, filename):
     line = matrix[0]
 
     file = decompress_string(line)
-    cleaned_string = reduce_string(file)
+    if part == 1:
+        cleaned_string = reduce_string(file)
+    else:
+        cleaned_string = reduce_string_part2(file)
     check_sum = calculate_check_sum(cleaned_string)
 
     print(check_sum)
 
 
-main(1, "input")
+main(2, "input")
